e4a1ea494fb6e5c40a0aee775fd038a3a4b05950
Ngoc Son Ninh <123456nuct@gmail.com>
2024-05-27 00:49:04 +0700

    Build the infrastructure

---
+++ src/Examination/Examination.Infrastructure/Repositories/ExamResultRepository.cs
@@ -0,0 +1,29 @@
+﻿using DocumentFormat.OpenXml.Math;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Examination.Domain.AggregateModels.ExamAggregate;
+using Examination.Domain.AggregateModels.ExamResultAggregate;
+using Examination.Infrastructure.SeedWork;
+using MediatR;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Infrastructure.Repositories
+{
+    public class ExamResultRepository : BaseRepository<ExamResult>, IExamResultRepository
+    {
+        public ExamResultRepository(IMongoClient mongoClient, IClientSessionHandle clientSessionHandle, IOptions<ExamSettings> settings, IMediator mediator, string collection) : base(mongoClient, clientSessionHandle, settings, mediator, collection)
+        {
+        }
+
+        public async Task<ExamResult> GetDetails(string userId, string examId)
+        {
+            var filter = Builders<ExamResult>.Filter.Where(s => s.ExamId == examId && s.UserId == userId);
+            return await Collection.Find(filter).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ src/Examination/Examination.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using Examination.Domain.AggregateModels.ExamAggregate;
+using Examination.Domain.AggregateModels.UserAggregate;
+using Examination.Infrastructure.SeedWork;
+using MediatR;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Infrastructure.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(IMongoClient mongoClient, IClientSessionHandle clientSessionHandle, IOptions<ExamSettings> settings, IMediator mediator, string collection) : base(mongoClient, clientSessionHandle, settings, mediator, collection)
+        {
+        }
+
+        public async Task<User> GetUserByIdAsync(string externalId)
+        {
+            var filter = Builders<User>.Filter.Eq(s => s.ExternalId, externalId);
+            return await Collection.Find(filter).FirstOrDefaultAsync();
+        }
+    }
+}
