ac866d636e9e7fb9a026a5109e58011e2469e628
Ngoc Son Ninh <123456nuct@gmail.com>
2024-05-31 23:29:19 +0700

    Create admin project with MudBlazor(Blazor Web Assembly)

---
+++ src/WebApps/AdminApp/Models/ChatMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminApp.Models
+{
+    public class ChatMessage
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
