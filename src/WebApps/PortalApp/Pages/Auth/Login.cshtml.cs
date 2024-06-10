08d9b7f6ed0046153a571ccbabba32be72a26a51
Ngoc Son Ninh <123456nuct@gmail.com>
2024-06-11 01:01:33 +0700

    integated Authntication with Identity Server

---
+++ src/WebApps/PortalApp/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortalApp.Core;
+
+namespace PortalApp.Pages.Auth
+{
+    public class LoginModel : PageModel
+    {
+        public async Task OnGetAsync()
+        {
+            await HttpContext.ChallengeAsync(AuthenticationConsts.OidcAuthenticationScheme, new AuthenticationProperties
+            {
+                RedirectUri= "/"
+            });
+        }
+    }
+}
